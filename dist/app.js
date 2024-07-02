2d70440ad3c3cb4c8a0a3032872102e7322578f7
FanisFFF <faskhutdinovfanis@gmail.com>
2024-07-02 16:58:18 +0300

    added remote backend

--- dist/app.js
+++ dist/app.js
@@ -9,13 +9,16 @@ const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const dataRoutes_1 = __importDefault(require("./routes/dataRoutes"));
+require("dotenv/config");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-mongoose_1.default.connect("mongodb://127.0.0.1:27017/spaapp", {
+console.log(process.env.CONNECTION_STRING);
+mongoose_1.default.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 app.use("/auth", authRoutes_1.default);
 app.use("/data", dataRoutes_1.default);
 exports.default = app;
+//
--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,22 @@ import cors from "cors";
 import mongoose, { ConnectOptions } from "mongoose";
 import authRoutes from "./routes/authRoutes";
 import dataRoutes from "./routes/dataRoutes";
-
+import "dotenv/config";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect("mongodb://127.0.0.1:27017/spaapp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-} as ConnectOptions);
+console.log(process.env.CONNECTION_STRING);
+mongoose.connect(
+  process.env.CONNECTION_STRING as string,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions
+);
 
 app.use("/auth", authRoutes);
 app.use("/data", dataRoutes);
 
 export default app;
+//
