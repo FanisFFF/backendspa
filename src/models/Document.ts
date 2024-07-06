cbed3f2d1aa9ac946b170c918688bc6bea39735d
FanisFFF <faskhutdinovfanis@gmail.com>
2024-07-06 15:33:41 +0300

    changed data input

--- src/models/Document.ts
+++ src/models/Document.ts
@@ -15,13 +15,14 @@ import { Schema, model, Document } from "mongoose";
 
 interface IDocument extends Document {
   companySigDate: string;
-  companySignatureName: string;
-  documentName: string;
-  documentStatus: string;
-  documentType: string;
-  employeeNumber: string;
-  employeeSigDate: string;
-  employeeSignatureName: string;
+  postText: string;
+  // companySignatureName: string;
+  // documentName: string;
+  // documentStatus: string;
+  // documentType: string;
+  // employeeNumber: string;
+  // employeeSigDate: string;
+  // employeeSignatureName: string;
   userId: string;
 }
 
@@ -38,13 +39,14 @@ interface IDocument extends Document {
 // });
 const documentSchema = new Schema<IDocument>({
   companySigDate: { type: String, required: true },
-  companySignatureName: { type: String, required: true },
-  documentName: { type: String, required: true },
-  documentStatus: { type: String, required: true },
-  documentType: { type: String, required: true },
-  employeeNumber: { type: String, required: true },
-  employeeSigDate: { type: String, required: true },
-  employeeSignatureName: { type: String, required: true },
+  postText: { type: String, required: true },
+  // companySignatureName: { type: String, required: true },
+  // documentName: { type: String, required: true },
+  // documentStatus: { type: String, required: true },
+  // documentType: { type: String, required: true },
+  // employeeNumber: { type: String, required: true },
+  // employeeSigDate: { type: String, required: true },
+  // employeeSignatureName: { type: String, required: true },
   userId: { type: String, required: true },
 });
 
